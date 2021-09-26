8a26ec5135f2e69c0522f3faffad6630d82fc334
dgafka <dgafka.mail@gmail.com>
2021-09-26 14:15:15 +0200

    begin

---
+++ src/main/java/com/dgafka/innerloop/InnerLoopApplication.java
@@ -0,0 +1,24 @@
+package com.dgafka.innerloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+public class InnerLoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnerLoopApplication.class, args);
+    }
+}
+
+@RestController
+class Controller {
+    @GetMapping("/")
+    String[] index() {
+        return new String[]{"Cricket", "Basketball", "Football", "Badminton", "RabbitMQ", "RabbitMQ", "RabbitMQ"};
+    }
+}
---
+++ src/test/java/com/dgafka/innerloop/InnerLoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgafka.innerloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerLoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
